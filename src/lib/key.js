4f613d8210e0f732bdff0d9e8a87d8aa81da7158
Dilan de la Rosa <drosay@unicartagena.edu.co>
2022-12-07 02:22:16 -0500

    another commit

--- src/lib/key.js
+++ src/lib/key.js
@@ -1,10 +1,10 @@
 'use strict'
 
 module.exports = {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "containers-us-west-151.railway.app",
     database: process.env.DB_NAME || "tasks",
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "12345",
-    port: process.env.DB_PORT || 3306,
+    password: process.env.DB_PASSWORD || "I7yhrlpymRT4KT03nkdC",
+    port: process.env.DB_PORT || 5487,
     insecureAuth: true
 }
